682c5978c9309770f0c6a42294e12570b9e8936e
WELLINGTON BARCELOS <wellingtonbarcelosdev@gmail.com>
2022-07-10 21:36:31 -0300

    adição porcentagem de defeituosos

--- carta de controle.py
+++ carta de controle.py
@@ -3,4 +3,7 @@ dados = cont = soma = 0
 while cont < amostra:
     cont += 1
     dados = float(input(f'Amostra {cont}: '))
+    defeituosos = int(input('Defeituosos: '))
+    porcentagem = (defeituosos * 100) / dados
+    print(f'Porcentagem de defeituosos: {porcentagem:.2f}%')
     soma += dados
