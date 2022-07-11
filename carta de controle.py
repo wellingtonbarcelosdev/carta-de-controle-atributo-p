c0340eaeed14907a4b727e4f98146cf349703992
WELLINGTON BARCELOS <wellingtonbarcelosdev@gmail.com>
2022-07-10 21:53:48 -0300

    adição de médias

--- carta de controle.py
+++ carta de controle.py
@@ -1,9 +1,14 @@
 amostra = int(input('Tamanho da amostra: '))
-dados = cont = soma = 0
+dados = cont = somadados = somadefeituosos = 0
 while cont < amostra:
     cont += 1
     dados = float(input(f'Amostra {cont}: '))
     defeituosos = int(input('Defeituosos: '))
     porcentagem = (defeituosos * 100) / dados
     print(f'Porcentagem de defeituosos: {porcentagem:.2f}%')
-    soma += dados
+    somadados += dados
+    somadefeituosos += defeituosos
+pmedia = (somadefeituosos / somadados) * 100
+amostramedia = (somadados / amostra)
+print(f'{pmedia:.2f}')
+print(f'{amostramedia:.2f}')
